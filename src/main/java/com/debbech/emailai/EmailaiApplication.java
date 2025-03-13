2bcadd1dab8932fc6f2ad77d5c36d106e4cf254e
ahmed-debbech <debbech.ahmed@gmail.com>
2025-03-13 12:54:20 +0100

    add initial set up

---
+++ src/main/java/com/debbech/emailai/EmailaiApplication.java
@@ -0,0 +1,13 @@
+package com.debbech.emailai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/debbech/emailai/EmailaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debbech.emailai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
